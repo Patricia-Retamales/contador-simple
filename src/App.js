1b107ff98814377c232e913ac610f0db6496bed2
patricia <retamalespatricia01@gmail.com>
2022-08-17 19:30:01 -0400

    contador

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+function App ( props)  {
+    let unit = props.seconds;
+    let ten = 0;
+    let hundred = 0;
+    let thousand = 0;
+    let tenThousand = 0;
+    let hunThousand = 0;
+    if(props.seconds > 9){
+        ten =   Math.trunc(props.seconds / 10);
+        unit = props.seconds % 10;
+        if (ten > 9){
+            hundred =  Math.trunc(ten / 10);
+            ten = ten % 10;
+            if(hundred > 9){
+                thousand = Math.trunc(hundred / 10);
+                hundred = hundred %  10;
+                if(thousand > 9){
+                    tenThousand = Math.trunc(thousand / 10);
+                    thousand = thousand % 10;
+                    if(tenThousand > 9){
+                        hunThousand = Math.trunc(tenThousand / 10);
+                        tenThousand = tenThousand % 10;
+                    }
+                }
+            }
+        }
+    }
+    return (
+        <div className="app d-flex justify-content-center">
+            
+             <div className="icon box"><i class="bi bi-clock"></i></div> 
+            <div className="counter box">{hunThousand}</div>
+            <div className="counter box">{tenThousand}</div>
+            <div className="counter box">{thousand}</div>
+            <div className="counter box">{hundred}</div>
+            <div className="counter box">{ten}</div>
+            <div className="counter box">{unit}</div>
+        </div>
+    );
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import App from './App.js';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let counter = 0;
+
+setInterval(function(){
+    ReactDOM.render(<App seconds={counter}/>, document.getElementById('root'));
+    counter += 1;
+},
+1000);
